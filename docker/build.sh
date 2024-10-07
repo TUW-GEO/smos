d6e9e15f23b55724e56fac26ee3773706abaf7bb
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2024-10-07 10:18:33 +0200

    Update docker URL

--- docker/build.sh
+++ docker/build.sh
@@ -12,7 +12,7 @@ fi
 # Tag/branch/commit in https://git.eodc.eu/cci-sm-work/cci_sm_ecvps_py_src
 GIT_BRANCH_TAG_COMMIT=$1
 
-GIT_URL="https://github.com/wpreimes/smos.git"
+GIT_URL="https://github.com/TUW-GEO/smos.git"
 
 echo "Calling Dockerfile at $this_dir/docker/Dockerfile"
 echo "Checking out source tag $GIT_BRANCH_TAG_COMMIT"
